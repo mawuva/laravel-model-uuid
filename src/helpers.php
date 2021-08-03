5146c8f26984cf111e06429d3a498891880e1ca4
Seddor Ephraim <seddorephraim7@gmail.com>
2021-08-03 01:43:21 +0100

    Add is_the_given_id_a_uuid function in helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -1,2 +1,43 @@
 <?php
 
+use Mawuekom\ModelUuid\Utils\ValidatesUuid;
+
+if (!function_exists('is_the_given_id_a_uuid')) {
+    /**
+     * Check if the givent id is a uuid
+     *
+     * @param string $uuidColumn
+     * @param string $uuid
+     * @param Illuminate\Database\Eloquent\Model $class
+     *
+     * @return bool
+     */
+    function is_the_given_id_a_uuid($uuidColumn, $uuid, $class): bool {
+        $data = (new class { 
+            use ValidatesUuid;
+
+            /**
+             * Resolve Uuid
+             *
+             * @param string $uuidColumn
+             * @param string $uuid
+             * @param Illuminate\Database\Eloquent\Model $class
+             *
+             * @return bool
+             */
+            public function resolveUuid($uuidColumn, $uuid, $class): bool {
+                try {
+                    $this ->validatesUuid($uuidColumn, $uuid, $class);
+                    return true;
+                }
+
+                catch (Exception $e) {
+                    //$msg = $e ->getMessage();
+                    return false;
+                }
+            }
+         });
+
+         return $data ->resolveUuid($uuidColumn, $uuid, $class);
+    }
+}
